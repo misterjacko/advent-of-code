14ffcd7c255c41cfc8fcdba8f89310f55882b8f3
Jakob Ondrey <ondrey@gmail.com>
2021-11-13 19:46:49 -0600

    Day 5

--- 2015/05/main.go
+++ 2015/05/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"crypto/md5"
+	"bufio"
 	"fmt"
 	"log"
-	"strconv"
+	"os"
 	"strings"
 	"time"
 )
@@ -15,44 +15,130 @@ func check(e error) {
 	}
 }
 
-func Md5(in string) string {
-	data := []byte(in)
-	return fmt.Sprintf("%x", md5.Sum(data))
+func LoadInput(fileName string) []string {
+	input := []string{}
+	file, err := os.Open(fileName)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		block := scanner.Text()
+		input = append(input, block)
+
+	}
+	return input
+}
+func NoForbiddenSubString(str string) bool {
+	response := true
+	forbiddenStrings := []string{
+		"ab",
+		"cd",
+		"pq",
+		"xy",
+	}
+	for _, fs := range forbiddenStrings {
+		if strings.Contains(str, fs) {
+			return false
+		}
+	}
+	return response
 }
 
-func Part1(input string) int {
-	i := 0
-	for {
-		iString := strconv.Itoa(i)
-		newString := fmt.Sprintf("%s%s", input, iString)
-		md5 := Md5(newString)
-		if strings.HasPrefix(md5, "00000") {
-			return i
-		} else {
-			i++
-		}
+func IsVowel(char string) int {
+	vowels := "aeiou"
+	if strings.Contains(vowels, char) {
+		return 1
+	} else {
+		return 0
 	}
 }
 
-func Part2(input string) int {
-	i := 0
-	for {
-		iString := strconv.Itoa(i)
-		newString := fmt.Sprintf("%s%s", input, iString)
-		md5 := Md5(newString)
-		if strings.HasPrefix(md5, "000000") {
-			return i
-		} else {
-			i++
+func IsDupe(pastChar string, charStr string) bool {
+	return pastChar == charStr
+}
+
+func Iterate(str string) bool {
+	vowelCount := 0
+	hasDouble := false
+	pastChar := ""
+	for _, char := range str {
+		charStr := string(char)
+		if vowelCount < 3 {
+			vowelCount += IsVowel(charStr)
+		}
+		if !hasDouble {
+			hasDouble = IsDupe(pastChar, charStr)
+		}
+		pastChar = charStr
+
+	}
+	if vowelCount >= 3 && hasDouble {
+		return true
+	}
+	return false
+}
+
+func Part1(data []string) int {
+	niceCount := 0
+	for _, str := range data {
+		if Iterate(str) && NoForbiddenSubString(str) {
+			niceCount += 1
 		}
 	}
+	return niceCount
+}
+func Array(str string) []string {
+	returnArray := []string{}
+	for _, char := range str {
+		returnArray = append(returnArray, string(char))
+	}
+	return returnArray
+}
+
+func Sandwich(input string) bool {
+	arrInput := Array(input)
+	for i := 0; i < len(arrInput)-2; i++ {
+		if arrInput[i] == arrInput[i+2] {
+			return true
+		}
+	}
+	return false
+}
+
+func TwoRepeat(input string) bool {
+	arrInput := Array(input)
+	charMap := map[string]int{}
+	for i := 0; i < len(arrInput)-1; i++ {
+		key := fmt.Sprintf("%s%s", arrInput[i], arrInput[i+1])
+		if index, ok := charMap[key]; ok {
+			if i-index > 1 {
+				return true
+			}
+		} else {
+			charMap[key] = i
+		}
+	}
+	return false
+}
+
+func Part2(data []string) int {
+	niceCount := 0
+	for _, str := range data {
+		if Sandwich(str) && TwoRepeat(str) {
+			niceCount += 1
+		}
+	}
+	return niceCount
 }
 
 func main() {
 	startTime := time.Now()
-	data := "yzbqklnj"
+	data := LoadInput("input.txt")
 	fmt.Printf("Part 1: %v\n", Part1(data))
 	fmt.Println(time.Since(startTime))
 	fmt.Printf("Part 2: %v\n", Part2(data))
 	fmt.Println(time.Since(startTime))
+
 }
--- 2015/05/main_test.go
+++ 2015/05/main_test.go
@@ -6,7 +6,60 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestLoadInput(t *testing.T) {
+	stringResult := []string{
+		"ugknbfddgicrmopn",
+		"aaa",
+		"jchzalrnumimnmhp",
+		"haegwjzuvuyypxyu",
+		"dvszwmarrgswjxmb",
+	}
+	assert.Equal(t, stringResult, LoadInput("input_test.txt"))
+}
+
+func TestNoForbiddenSubString(t *testing.T) {
+	assert.Equal(t, true, NoForbiddenSubString("ugknbfddgicrmopn"))
+	assert.Equal(t, false, NoForbiddenSubString("haegwjzuvuyypxyu"))
+}
+
+func TestIsVowel(t *testing.T) {
+	assert.Equal(t, 1, IsVowel("a"))
+	assert.Equal(t, 1, IsVowel("u"))
+	assert.Equal(t, 0, IsVowel("t"))
+}
+
+func TestIsDupe(t *testing.T) {
+	assert.Equal(t, true, IsDupe("a", "a"))
+	assert.Equal(t, false, IsDupe("", "a"))
+	assert.Equal(t, false, IsDupe("a", "b"))
+}
+
+func TestIterate(t *testing.T) {
+	assert.Equal(t, true, Iterate("ugknbfddgicrmopn"))
+	assert.Equal(t, false, Iterate("dvszwmarrgswjxmb"))
+	assert.Equal(t, false, Iterate("jchzalrnumimnmhp"))
+}
+
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 609043, Part1("abcdef"))
-	assert.Equal(t, 1048970, Part1("pqrstuv"))
+	data := LoadInput("input_test.txt")
+	assert.Equal(t, 2, Part1(data))
+}
+
+func TestArray(t *testing.T) {
+	assert.Equal(t, []string{"q", "j", "h", "v"}, Array("qjhv"))
+}
+
+func TestSandwich(t *testing.T) {
+	assert.Equal(t, true, Sandwich("qjhvhtzxzqqjkmpb"))
+	assert.Equal(t, false, Sandwich("uurcxstgmygtbstg"))
+}
+
+func TestTwoRepeat(t *testing.T) {
+	assert.Equal(t, true, TwoRepeat("qjhvhtzxzqqjkmpb"))
+	assert.Equal(t, false, TwoRepeat("ieodomkazucvgmuy"))
+}
+
+func TestPart2(t *testing.T) {
+	data := LoadInput("input_test2.txt")
+	assert.Equal(t, 2, Part2(data))
 }
