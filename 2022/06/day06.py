933be79369a54673232f5134097125d28bab9964
Jakob <jakob.ondrey@yello.co>
2022-12-06 10:02:01 -0600

    refactor

--- 2022/06/day06.py
+++ 2022/06/day06.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Prep Work #
 #############
 
-def loadData(dataFile):
+def loadData(dataFile: str):
     with open(dataFile, "r") as f:
         lines = f.readlines()
     return [line.strip() for line in lines][0]
@@ -18,10 +18,13 @@ def test_loadData():
 # Part 1 #
 ##########
 
-def part1(data):
-    for i in range(len(data)):
-        if len(set(data[i:i+4])) == 4:
-            return i+4
+def find_index(data:str, window: int):
+    for i in range(window-1, len(data)):
+        if len(set(data[i-window:i])) == window:
+            return i
+
+def part1(data: str):
+    return find_index(data, 4)
             
 def test_part1():
     testData = "test_input.txt"
@@ -36,10 +39,8 @@ def test_part1():
 # Part 2 #
 ##########
 
-def part2(data):
-    for i in range(len(data)):
-        if len(set(data[i:i+14])) == 14:
-            return i+14
+def part2(data: str):
+    return find_index(data, 14)
 
 def test_part2():
     testData = "test_input.txt"
